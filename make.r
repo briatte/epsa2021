75602ef45a437ec47a6d0be51ac09ef2d80507df
François Briatte <briatte@gmail.com>
2021-07-27 18:52:10 +0200

    Update make.r

--- make.r
+++ make.r
@@ -1,9 +1,11 @@
 # download and preprocess
 source("01-download.r")
 source("02-sessions.r")
-source("03-abstracts.r")
 
 # example graph
-source("04-network.r")
+source("03-network.r")
 
-# wip
+# extract full program information
+source("04-full-program.r")
+
+# kthxbye
